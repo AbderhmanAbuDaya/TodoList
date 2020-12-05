8612ecf73b83da6b6ac5cbfe8ec3858836cb66fb
Abderhman Daya <lel23443217399@gmail.com>
2020-12-06 00:39:42 +0200

    second commit

--- app/src/main/java/com/example/todollist/MainActivity.java
+++ app/src/main/java/com/example/todollist/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.todollist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView NextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NextBtn=findViewById(R.id.NextBtn);
+        NextBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
